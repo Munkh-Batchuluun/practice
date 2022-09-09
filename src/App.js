41d6dcef4cfed542dd1b07cc8a84d049149c136f
Munkh-Batchuluun <munkh.bc@gmail.com>
2022-09-09 11:53:32 +1000

    some changes

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
       <h1>COMP3120: Advanced Wed Development</h1> 
       <Form updateFn={addNewUnit}></Form>        
       <Unit data={units} deleteFn={deleteUnit}></Unit>
-      <h1>This is change</h1>
+      <h1>This is change e</h1>
     </div>
   );
 }
